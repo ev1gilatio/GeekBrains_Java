9ddd63aab3e42410842d3e1457ec3905bc92a8c6
Samarin Vladimir <84005587+ev1gilatio@users.noreply.github.com>
2021-06-02 12:03:20 +0300

    lvl1_lesson_6

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Animal wolf = new Animal("Boo");
+        Cat cat = new Cat("Barsik");
+        Dog dog = new Dog("Sharik");
+    }
+}
